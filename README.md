502c2490518485c1369178acaa69e8ed750e444b
shoma564 <shomaigu@gmail.com>
2023-09-04 23:17:53 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,120 @@
+## 目的
+コンテナにおけるメリットの一つは可搬性である。LXDはVMよりは軽量ではあるがDockerコンテナ程軽量ではない。そのため、可搬性が失われつつある。
+そこで、Dokcerの様にどこでも同じ様にビルドが出来れば可搬性が上がるのではないかと考えた。
+
+## 配置場所
+本コードはgithubにも配置している
+
+
+## 使い方
+lxdcliには3つの命令文がある。
+```bash
+$ lxdcli
+
+Usage: lxdcli COMMAND
+
+
+Common Commands:
+  build    build a lxdfile
+  copy     copy containers
+  delete   delete containers
+```
+
+### build
+buildはlxdfileに基づいて、lxdコンテナを作成する。また、引数にlxdfileを指定しなければならない。
+
+```bash
+root@shoma:/home/shoma/lxdcli/sample # lxdcli build
+
+lxdfileを指定してください
+
+
+root@shoma:/home/shoma/lxdcli/sample # lxdcli build ./lxdfile
+>>>>>>>> lxc launch images:ubuntu/23.10 ubuntu-lamp
+
+
+Error: Failed instance creation: Failed creating instance record: Add instance info to the database: This "instances" entry already exists
+Creating ubuntu-lamp
+
+>>>>>>>> lxc start ubuntu-lamp
+
+
+Error: The instance is already running
+```
+
+### copy
+copyは既に存在するLXDコンテナを複数個コピーする。引数には、コンテナ名と、コンテナの数を指定する必要がある。
+
+```bash
+root@shoma:/home/shoma/lxdcli/sample # lxdcli copy
+
+コピーするコンテナ名を指定してください
+root@shoma:/home/shoma/lxdcli/sample # lxdcli copy ubuntu-lamp
+
+コピーするコンテナ数を指定してください
+root@shoma:/home/shoma/lxdcli/sample # lxdcli copy ubuntu-lamp 2
+>>>>>>>> lxc copy ubuntu-lamp ubuntu-lamp-0
+```
+
+```bash
+root@shoma:/home/shoma/lxdcli/sample # lxc list
++---------------+---------+------------------------------+-----------------------------------------------+-----------+-----------+
+|     NAME      |  STATE  |             IPV4             |                     IPV6                      |   TYPE    | SNAPSHOTS |
++---------------+---------+------------------------------+-----------------------------------------------+-----------+-----------+
+| alma-lamp     | RUNNING | 10.107.73.31 (eth0)          | fd42:d70a:2761:a81b:216:3eff:fee3:7476 (eth0) | CONTAINER | 2         |
++---------------+---------+------------------------------+-----------------------------------------------+-----------+-----------+
+| ubuntu-lamp   | RUNNING | 172.19.0.1 (br-1b82b9c5b955) | fd42:d70a:2761:a81b:216:3eff:fe9d:ab36 (eth0) | CONTAINER | 0         |
+|               |         | 172.17.0.1 (docker0)         |                                               |           |           |
+|               |         | 10.107.73.158 (eth0)         |                                               |           |           |
++---------------+---------+------------------------------+-----------------------------------------------+-----------+-----------+
+| ubuntu-lamp-0 | RUNNING | 172.19.0.1 (br-1b82b9c5b955) | fd42:d70a:2761:a81b:216:3eff:fe6e:a670 (eth0) | CONTAINER | 0         |
+|               |         | 172.17.0.1 (docker0)         |                                               |           |           |
+|               |         | 10.107.73.112 (eth0)         |                                               |           |           |
++---------------+---------+------------------------------+-----------------------------------------------+-----------+-----------+
+| ubuntu-lamp-1 | RUNNING | 172.19.0.1 (br-1b82b9c5b955) | fd42:d70a:2761:a81b:216:3eff:fe28:393a (eth0) | CONTAINER | 0         |
+|               |         | 172.17.0.1 (docker0)         |                                               |           |           |
+|               |         | 10.107.73.79 (eth0)          |                                               |           |           |
++---------------+---------+------------------------------+-----------------------------------------------+-----------+-----------+
+```
+
+### delete
+deleteは主にcopyで生成されたコンテナを削除するのに使用する。引数にコンテナ名とコンテナの数を指定する必要がある。
+```bash
+root@shoma:/home/shoma/lxdcli/sample # lxdcli delete
+
+削除するコンテナ名を指定してください
+root@shoma:/home/shoma/lxdcli/sample # lxdcli delete ubuntu-lamp
+
+削除するコンテナ数を指定してください
+root@shoma:/home/shoma/lxdcli/sample # lxdcli delete ubuntu-lamp 3
+>>>>>>>> lxc delete ubuntu-lamp-0 --force
+```
+```bash
++-------------+---------+------------------------------+-----------------------------------------------+-----------+-----------+
+|    NAME     |  STATE  |             IPV4             |                     IPV6                      |   TYPE    | SNAPSHOTS |
++-------------+---------+------------------------------+-----------------------------------------------+-----------+-----------+
+| alma-lamp   | RUNNING | 10.107.73.31 (eth0)          | fd42:d70a:2761:a81b:216:3eff:fee3:7476 (eth0) | CONTAINER | 2         |
++-------------+---------+------------------------------+-----------------------------------------------+-----------+-----------+
+| ubuntu-lamp | RUNNING | 172.19.0.1 (br-1b82b9c5b955) | fd42:d70a:2761:a81b:216:3eff:fe9d:ab36 (eth0) | CONTAINER | 0         |
+|             |         | 172.17.0.1 (docker0)         |                                               |           |           |
+|             |         | 10.107.73.158 (eth0)         |                                               |           |           |
++-------------+---------+------------------------------+-----------------------------------------------+-----------+-----------+
+```
+
+
+## LXDfileの書き方
+LXDfileには```CONTAINERNAME```が必須の命令文である。また、```CONTAINERNAME```と```FROM```は一番最初に配置しなければならない。
+
+例
+```bash
+CONTAINERNAME ubuntu-lamp
+FROM ubuntu/23.10
+
+RUN apt -y update
+RUN apt -y install docker.io docker-compose
+ADD ./docker-compose.yml /etc/
+RUN docker-compose -f /etc/docker-compose.yml up -d
+RUN docker ps
+
+NUMBER 2
+```
---
+++ sample/lxdcli.py
@@ -0,0 +1,167 @@
+#!/usr/bin/env python3
+import re
+import subprocess
+import sys
+import time
+
+args = sys.argv
+
+if len(args) < 2:
+    print("\nUsage: lxdcli COMMAND\n\n")
+    print("Common Commands:")
+    print("  build    build a lxdfile")
+    print("  copy     copy containers")
+    print("  delete   delete containers")
+    print("\n\n\n")
+    sys.exit()
+
+if args[1] == "delete":
+    if len(args) < 3:
+        print("\n削除するコンテナ名を指定してください")
+        sys.exit()
+
+        containername = "your_container_name_here"  # Replace with the actual container name
+    elif len(args) < 4:
+        print("\n削除するコンテナ数を指定してください")
+        sys.exit()
+    
+    containername = args[2]
+
+    number = args[3]
+    number = int(number)
+    if number > 0:
+        for k in range(number):
+            command = "lxc delete " + str(containername) + "-" + str(k) + " --force"
+            print(">>>>>>>> " + str(command) + "\n\n")
+            process = (subprocess.Popen(command, stdout=subprocess.PIPE, shell=True).communicate()[0]).decode('utf-8')
+            print(process)
+
+            time.sleep(3)
+
+        command = "lxc list"
+        process = (subprocess.Popen(command, stdout=subprocess.PIPE, shell=True).communicate()[0]).decode('utf-8')
+        print(process)
+
+
+if args[1] == "copy":
+    if len(args) < 3:
+        print("\nコピーするコンテナ名を指定してください")
+        sys.exit()
+
+        containername = "your_container_name_here"  # Replace with the actual container name
+    elif len(args) < 4:
+        print("\nコピーするコンテナ数を指定してください")
+        sys.exit()
+    
+    containername = args[2]
+
+    number = args[3]
+    number = int(number)
+    if number > 0:
+        for k in range(number):
+            command = "lxc copy " + str(containername) + " " + str(containername) + "-" + str(k)
+            print(">>>>>>>> " + str(command) + "\n\n")
+            process = (subprocess.Popen(command, stdout=subprocess.PIPE, shell=True).communicate()[0]).decode('utf-8')
+            print(process)
+            command = "lxc start " + str(containername) + "-" + str(k)
+            print(">>>>>>>> " + str(command) + "\n\n")
+            process = (subprocess.Popen(command, stdout=subprocess.PIPE, shell=True).communicate()[0]).decode('utf-8')
+            print(process)
+
+            time.sleep(3)
+
+        command = "lxc list"
+        process = (subprocess.Popen(command, stdout=subprocess.PIPE, shell=True).communicate()[0]).decode('utf-8')
+        print(process)
+
+
+
+elif args[1] == "build":
+    if len(args) < 3:
+        print("\nlxdfileを指定してください\n\n")
+        sys.exit()
+
+    i = 0
+    with open(args[2], 'r') as file:
+        for line in file:
+            match = re.match(r'(\S+)\s+(.*)', line)
+            if match:
+                first_word = match.group(1)
+                if first_word == "CONTAINERNAME":
+                    i = i + 1
+                else:
+                    pass
+    if i == 0:
+        print("ERROR: please define containername\n\n")
+        sys.exit()
+
+    u = 0
+    with open('./lxdfile', 'r') as file:
+        for line in file:
+            match = re.match(r'(\S+)\s+(.*)', line)
+            if match:
+                first_word = match.group(1)
+                if first_word == "NUMBER":
+                    u = u + 1
+                else:
+                    pass
+    if u == 0:
+        number = 0
+
+    containername = ""  # Initialize containername
+    with open('./lxdfile', 'r') as file:
+        for line in file:
+            time.sleep(1)
+            match = re.match(r'(\S+)\s+(.*)', line)
+            if match:
+                first_word = match.group(1)
+                remaining_words = match.group(2)
+                if first_word == "CONTAINERNAME":
+                    containername = remaining_words
+                elif first_word == "NUMBER":
+                    number = remaining_words
+                elif first_word == "FROM":
+                    command = "lxc launch images:" + str(remaining_words) + " " + str(containername)
+                    command2 = "lxc start " + str(containername)
+                    print(">>>>>>>> " + str(command) + "\n\n")
+                    process = (subprocess.Popen(command, stdout=subprocess.PIPE, shell=True).communicate()[0]).decode('utf-8')
+                    print(process)
+
+                    print(">>>>>>>> " + str(command2) + "\n\n")
+                    process = (subprocess.Popen(command2, stdout=subprocess.PIPE, shell=True).communicate()[0]).decode('utf-8')
+                    print(process)
+
+                    time.sleep(1)
+
+                elif first_word == "RUN":
+                    command = "lxc exec " + str(containername) + " -- " + str(remaining_words)
+                    print(">>>>>>>> " + str(command) + "\n\n")
+                    process = (subprocess.Popen(command, stdout=subprocess.PIPE, shell=True).communicate()[0]).decode('utf-8')
+                    print(process)
+
+                elif first_word == "ADD":
+                    words = line.split()
+                    if words:
+                        command = "lxc file push " + words[1] + " " + str(containername) + "/" + words[2]
+                        print(">>>>>>>> " + str(command) + "\n\n")
+                        process = (subprocess.Popen(command, stdout=subprocess.PIPE, shell=True).communicate()[0]).decode('utf-8')
+                        print(process)
+
+    number = int(number)
+    if number > 0:
+        for k in range(number):
+            command = "lxc copy " + str(containername) + " " + str(containername) + "-" + str(k)
+            print(">>>>>>>> " + str(command) + "\n\n")
+            process = (subprocess.Popen(command, stdout=subprocess.PIPE, shell=True).communicate()[0]).decode('utf-8')
+            print(process)
+
+            command = "lxc start " + str(containername) + "-" + str(k)
+            print(">>>>>>>> " + str(command) + "\n\n")
+            process = (subprocess.Popen(command, stdout=subprocess.PIPE, shell=True).communicate()[0]).decode('utf-8')
+            print(process)
+
+    time.sleep(3)
+
+    command = "lxc list"
+    process = (subprocess.Popen(command, stdout=subprocess.PIPE, shell=True).communicate()[0]).decode('utf-8')
+    print(process)
